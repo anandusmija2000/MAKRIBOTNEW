8f2cc54ba92eca7790b4ec8b089f52a1a62d29eb
anandusmija2000 <89525378+anandusmija2000@users.noreply.github.com>
2021-08-26 12:27:46 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/searchandtakemovie'),
+        InlineKeyboardButton('Developers', url='https://t.me/Crazybot'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/searchandtakemovie')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/Crazybot')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
